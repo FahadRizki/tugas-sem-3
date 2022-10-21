0b37b7b8d31da76a373cf2eb818cdc0224ec507f
FahadRizki <114449269+FahadRizki@users.noreply.github.com>
2022-10-21 07:02:37 +0700

    tugas 4

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+package TugasSem3;
+
+public class Main {
+    public static void main(String[] args) {
+         Hewan hewanSaya = new Hewan();  // membuat object hewan
+         Hewan tikusSaya = new Tikus();  // membuat object tikus
+         Hewan AnjingSaya = new Anjing(); // membuat object kucing
+         hewanSaya.suaraHewan();
+         tikusSaya.suaraHewan();
+         AnjingSaya.suaraHewan();
+     }
+    
+}
