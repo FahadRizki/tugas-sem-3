583be092b4ea57c6080c3b9e761666ba769519a7
FahadRizki <114449269+FahadRizki@users.noreply.github.com>
2022-12-24 07:29:22 +0700

    Add files via upload

---
+++ genericNo2/game/Hero.java
@@ -0,0 +1,11 @@
+
+package genericNo2.game;
+
+public abstract class Hero {
+     abstract String getName();
+    abstract float getHp();
+    abstract float getAttack();
+    abstract float getDefense();
+    public abstract void infoHero();
+    
+}
